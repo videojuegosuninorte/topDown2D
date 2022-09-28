fc1198a7682c7c16bd8c0cef17fe55c78f75a45d
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-09-28 15:15:26 -0500

    Step1

---
+++ Assets/Scripts/TerrainScene/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int initialX = 0, initialY = 0;
+    public int speed = 1;
+    int direction = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MoveForward()
+    {
+
+    }
+
+    void MoveTo(int x, int y)
+    {
+
+    }
+}
